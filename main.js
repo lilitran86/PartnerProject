a9559c181b28a7a25215460a80bd374dda7a244c
lilitran86 <lilitran86@gmail.com>
2017-03-28 01:16:24 -0700

    updated framework project

--- main.js
+++ main.js
@@ -15,6 +15,100 @@ var today = year + "-" + month + "-" + day;
 document.getElementById('myDate').value = today;
 }
 
+//This is the api for eventful -- select id="activities"
+
+console.log($);
+
+$("#myBtn").click(function(){
+//	add text to the modal
+	$('#events').append('Your Events For the Day Go HERE');
+
+});
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+//
+
+$("#activities").click(function(){
+       $.ajax({
+       type: "GET",
+		xhrFields: {withCredentials: true},
+//       data:{'submit': true},
+       url: "http://api.eventful.com/json/events/search?&keywords=music&location=Los+Angeles&date=Future&app_key=qtzhbLdNnWNtZL7z",
+       dataType: "json",
+       success: function(events){
+       alert("Hello!");
+     }
+  });
+});
+
+//
+//
+////THis is the jquery
+//
+//$("#new-itinerary").click(function(){
+// var itinerary = $('<div>hey</div>');
+// $('#agenda').append(itinerary);
+//});
+//
+
+
+
+
+//
+//
+//
+//
+//function SetDate()
+//{
+//var date = new Date();
+//
+//var day = date.getDate();
+//var month = date.getMonth() + 1;
+//var year = date.getFullYear();
+//
+//if (month < 10) month = "0" + month;
+//if (day < 10) day = "0" + day;
+//
+//var today = year + "-" + month + "-" + day;
+//
+//
+//document.getElementById('myDate').value = today;
+//}
+
 
 
 // This is where the jquery for the sample begins
