fa47ff78edaba12fcd0e49c4c18165319e915f95
lilitran86 <lilitran86@gmail.com>
2017-03-29 15:37:07 -0700

    updated googlemaps

--- main.js
+++ main.js
@@ -55,72 +55,141 @@ window.onclick = function(event) {
 }
 
 //google maps 1
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -33.8688, lng: 151.2195},
-      zoom: 13
-    });
-    var input = document.getElementById('searchInput');
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// function initMap() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: -33.8688, lng: 151.2195},
+//       zoom: 13
+//     });
+//     var input = document.getElementById('searchInput');
+//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+//     autocomplete.bindTo('bounds', map);
+//
+//     var infowindow = new google.maps.InfoWindow();
+//     var marker = new google.maps.Marker({
+//         map: map,
+//         anchorPoint: new google.maps.Point(0, -29)
+//     });
+//
+//     autocomplete.addListener('place_changed', function() {
+//         infowindow.close();
+//         marker.setVisible(false);
+//         var place = autocomplete.getPlace();
+//         if (!place.geometry) {
+//             window.alert("Autocomplete's returned place contains no geometry");
+//             return;
+//         }
+//
+//         // If the place has a geometry, then present it on a map.
+//         if (place.geometry.viewport) {
+//             map.fitBounds(place.geometry.viewport);
+//         } else {
+//             map.setCenter(place.geometry.location);
+//             map.setZoom(17);
+//         }
+//         marker.setIcon(({
+//             url: place.icon,
+//             size: new google.maps.Size(71, 71),
+//             origin: new google.maps.Point(0, 0),
+//             anchor: new google.maps.Point(17, 34),
+//             scaledSize: new google.maps.Size(35, 35)
+//         }));
+//         marker.setPosition(place.geometry.location);
+//         marker.setVisible(true);
+//
+//         var address = '';
+//         if (place.address_components) {
+//             address = [
+//               (place.address_components[0] && place.address_components[0].short_name || ''),
+//               (place.address_components[1] && place.address_components[1].short_name || ''),
+//               (place.address_components[2] && place.address_components[2].short_name || '')
+//             ].join(' ');
+//         }
+//
+//         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+//         infowindow.open(map, marker);
+//
+//         //Location details
+//         for (var i = 0; i < place.address_components.length; i++) {
+//             if(place.address_components[i].types[0] == 'postal_code'){
+//                 document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+//             }
+//             if(place.address_components[i].types[0] == 'country'){
+//                 document.getElementById('country').innerHTML = place.address_components[i].long_name;
+//             }
+//         }
+//         document.getElementById('location').innerHTML = place.formatted_address;
+//         document.getElementById('lat').innerHTML = place.geometry.location.lat();
+//         document.getElementById('lon').innerHTML = place.geometry.location.lng();
+//     });
+// }
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
+var map;
+var infowindow;
 
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
-    });
+var request;
+var service;
+var markers = [];
 
-    autocomplete.addListener('place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            window.alert("Autocomplete's returned place contains no geometry");
-            return;
-        }
+function initialize() {
+  var center = new google.maps.LatLng(34.0432782,-118.2850588);
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
-        marker.setIcon(({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: 13
+  });
 
-        var address = '';
-        if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
+  request = {
+    location: center,
+    radius: 8047,
+    types: ['cafe','bakery','restaurant','bar']
+  };
+  infowindow = new google.maps.InfoWindow();
 
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
+  service = new google.maps.places.PlacesService(map);
 
-        //Location details
-        for (var i = 0; i < place.address_components.length; i++) {
-            if(place.address_components[i].types[0] == 'postal_code'){
-                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-            }
-            if(place.address_components[i].types[0] == 'country'){
-                document.getElementById('country').innerHTML = place.address_components[i].long_name;
-            }
-        }
-        document.getElementById('location').innerHTML = place.formatted_address;
-        document.getElementById('lat').innerHTML = place.geometry.location.lat();
-        document.getElementById('lon').innerHTML = place.geometry.location.lng();
-    });
+  service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map,'rightclick', function(event){
+    map.setCenter(event.latLng)
+    clearResults(markers)
+
+    var request = {
+      location: event.latLng,
+      radius: 8047,
+      types: ['cafe']
+    };
+    service.nearbySearch(request, callback);
+  })
 }
+function callback(results,status){
+  if(status == google.maps.places.PlacesServiceStatus.OK){
+    for(var i = 0; i < results.length; i++){
+      markers.push(createMarker(results[i]));
+    }
+  }
+}
+function createMarker(place){
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker,'click', function(){
+    infowindow.setContent(place.name);
+    infowindow.open(map,this);
+  });
+  return marker;
+
+}
+
+  function clearResults(markers){
+    for (var m in markers) {
+      markers[m].setMap(null)
+    }
+    markers = []
+  }
+  
+google.maps.event.addDomListener(window,'load',initialize);
